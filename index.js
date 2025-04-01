0cc0f7b614f8afae2bb6ec5aeedd85b7b0164dd1
lwam <lwambsrat@gmail.com>
2025-04-01 16:09:11 +0300

    added file

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ console.log({newState});
 
 
 //no.2 add char at 3 rd index
-let fname='I am Lwam Bisrat Weldehawreriat';
+let fname='I am Lwam Bisrat Weldehaweriat';
 const addFname=fname.slice(0, 3) + "y" + fname.slice(3);
-console.log(addFname);
+console.log({addFname});
 
 
 
@@ -16,13 +16,13 @@ console.log(addFname);
 let greet= "hello world, hello JavaScript!";
 const fgreet = "hello";
 const count = (greet.match(new RegExp(fgreet, "g")) || []).length;
-console.log(count);
+console.log({count});
 
 
 //no.4 delete last 3 char
 let school='AkiraChix is found in Kenya';
 const lastSchool=school.slice(-3);
-console.log(lastSchool);
+console.log({lastSchool});
 //or   console.log(school[school.length-3],school[school.length-2],school[schoo;.length-1]);
 
 
@@ -30,7 +30,7 @@ console.log(lastSchool);
 //no.5 delete first 3 char
 let  favourite='Chapati is favourite food in AkiraChix ';
 const firstFavourite=favourite.slice(0,3);
-console.log(firstFavourite);
+console.log({firstFavourite});
 // or console.log(favourite[0],favourite[1],favourite[2]);
 
 
@@ -38,26 +38,26 @@ console.log(firstFavourite);
 //no.6 ascending
 let book='trivor noah ,the power of now,rich dad poor dad'; // the spaces and commas(,) also ascend
 const ascendBook=book.split('').sort().join('');
-console.log(ascendBook);
+console.log({ascendBook});
 
 
 
 //7 delete spaces
 let text = "My favourite subjects are front and backend web development.";
 const  cleanedText = text.split(' ').join('');
-console.log(cleanedText);
+console.log({cleanedText});
 
 
 
 //no.8 index of a char
 let word="greeting";
 const middleWord=word.indexOf('t');
-console.log(middleWord);
+console.log({middleWord});
 
 //no.9 delete first 2 char
 let special='Special character';
 const delSpecial=special.slice(2);
-console.log(delSpecial);
+console.log({delSpecial});
 
 
 //no.10 delete last 2 char
@@ -65,7 +65,7 @@ let given='Thank you';
 const delGiven=given.slice(0,given.length-2);
 //or  const delGiven=given.substring(0,given.length-2);
 //or const delGiven =given.slice(0, -2)
-console.log(delGiven);
+console.log({delGiven});
 
 
 
